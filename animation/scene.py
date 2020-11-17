759de3f2b3610090ba2cbe67e90d5c7145f4163b
Jason Huang <jasonhuang20035@gmail.com>
2020-11-16 19:03:29 -0500

    Blue is a nice color

--- animation/scene.py
+++ animation/scene.py
@@ -3,17 +3,17 @@ from manim import *
 
 class AnimateLogo(Scene):
     def construct(self):
-        logo = Tex("\\hspace{3em}C", "lub \\\\", "JFS", "S")
-        logoGrey = logo.copy().set_color("#AAAAAA")
+        logo = Tex("\\hspace{3em}C", "lub \\\\", "JFS", "S").set_color(BLUE)
+        logoBg = logo.copy().set_color(WHITE)
         # Make it look like the entirety of Club is being written
-        logoGrey[0].set_color(WHITE)
-        group = VGroup(logo, logoGrey)
+        logoBg[0].set_color(BLUE)
+        group = VGroup(logo, logoBg)
         group.set_width(config["frame_width"] - 2 * LARGE_BUFF)
-        self.play(Write(logoGrey[2:4]))
+        self.play(Write(logoBg[2:4]))
         self.play(Write(logo[::len(logo)-1]))
-        self.play(Write(logoGrey[0:2]))
-        self.remove(logoGrey[3], logoGrey[0])   # Remove unneeded logo parts
+        self.play(Write(logoBg[0:2]))
+        self.remove(logoBg[3], logoBg[0])   # Remove unneeded logo parts
         self.wait()
-        self.play(FadeOutAndShift(logoGrey[1:3]),
+        self.play(FadeOutAndShift(logoBg[1:3]),
                   FadeOutAndShift(logo[::len(logo)-1]))
         self.wait()
